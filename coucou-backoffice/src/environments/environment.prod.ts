aa335057c759727d66c1c472b664a2bc16f7f5ff
Bilel Mekrazi <bilelmek@gmail.com>
2022-07-06 00:02:44 +0200

    fixs

--- coucou-backoffice/src/environments/environment.prod.ts
+++ coucou-backoffice/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true ,
-  url: "http://localhost:3006" ,
-  url_v2: "http://localhost:8080" ,
+  url: "https://www.coco.backoffice.cocotunisia.tn" ,
 };
--- coucou-backoffice/src/environments/environment.ts
+++ coucou-backoffice/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false ,
   url: "http://localhost:3006" ,
-  url_v2: "http://localhost:8080" ,
 };
