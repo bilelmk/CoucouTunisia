ba9cf78b29f6c29f4954fe45d23ddacc042b8fe1
Bilel Mekrazi <bilelmek@gmail.com>
2022-07-18 01:20:10 +0200

    fix sms message

--- coucou-backoffice/src/app/features/cb-main/cb-restaurants/cb-restaurants-add/cb-restaurants-add.component.ts
+++ coucou-backoffice/src/app/features/cb-main/cb-restaurants/cb-restaurants-add/cb-restaurants-add.component.ts
@@ -268,6 +268,7 @@ export class CbRestaurantsAddComponent implements OnInit {
       longitude: this.marker._latlng.lng ,
       latitude: this.marker._latlng.lat
     };
+    console.log(restaurant)
     this.formData.append('restaurant', JSON.stringify(restaurant));
     this.restaurantService.add(this.formData).subscribe(
       res => {
